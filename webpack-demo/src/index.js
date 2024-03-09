27fda74263f597f785e1a403cf2f72ba5891b52f
NiuBen <niuben@qq.com>
2024-03-09 20:17:04 +0800

    feat: 卸载资源管理相关

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,8 +1,4 @@
 import _ from 'lodash';
-import './style.css'
-import Icon from './icon.png'
-import Data from './data.xml';
-import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
@@ -10,16 +6,6 @@ function component() {
   // 执行这一行需要引入 lodash（目前通过 script 脚本引入）--不在需要
   // lodash 现在使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // 将图像添加到已经存在的 div 中。
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
 
   return element;
 }
