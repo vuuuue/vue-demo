b300545a2be6feb8dfd8a49fcdf62de8179d8846
NiuBen <niuben@qq.com>
2024-03-09 20:48:31 +0800

    feat: 自动编译代码

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,20 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js'
   },
   // entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
   plugins:[
     new HtmlWebpackPlugin({
-      title: '管理输出'
+      title: 'Development'
     })
   ],
   output: {
     filename: '[name].bundle.js',
     // filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
-  }
+    clean: true,
+    publicPath: '/'
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
