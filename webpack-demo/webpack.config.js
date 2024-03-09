c3aea716df4c83652c07baa9b7762a29f86f70c2
NiuBen <niuben@qq.com>
2024-03-09 21:28:22 +0800

    feat: splitChunks

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,21 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   // entry: './src/index.js',
-  // entry:{
-  //   index: './src/index.js',
-  //   another: './src/another-module.js',
-  // },
   entry:{
-    index: {
-      import: './src/index.js',
-      dependOn: ['sharedcs']
-    },
-    another: {
-      import: './src/another-module.js',
-      dependOn: ['sharedcs']
-    },
-    sharedcs: ['lodash'],
+    index: './src/index.js',
+    another: './src/another-module.js',
   },
+  // entry:{
+  //   index: {
+  //     import: './src/index.js',
+  //     dependOn: ['sharedcs']
+  //   },
+  //   another: {
+  //     import: './src/another-module.js',
+  //     dependOn: ['sharedcs']
+  //   },
+  //   sharedcs: ['lodash'],
+  // },
   plugins:[
     new HtmlWebpackPlugin({
       title: '代码分离'
@@ -29,6 +29,8 @@ module.exports = {
     clean: true
   },
   optimization: {
-    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+    },
   },
 };
