af855c6df27cdd2398c52a247eceb7142fced9e8
NiuBen <niuben@qq.com>
2024-03-18 10:07:09 +0800

    feat: 测试

--- vue3-demo/src/App.vue
+++ vue3-demo/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
+  <header :cs="'cscs'">
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
--- vue3-demo/src/main.ts
+++ vue3-demo/src/main.ts
@@ -2,5 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+const app = createApp(App)
+app.mount('#app')
 
-createApp(App).mount('#app')
+app.config.errorHandler = (err) => {
+  console.log('>>>>')
+  console.error(err)
+  console.log('<<<')
+}
+
