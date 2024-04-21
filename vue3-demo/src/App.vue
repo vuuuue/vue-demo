ba0c3abee94e8e937972656b4b83ad143af53cc6
NiuBen <niuben@qq.com>
2024-04-21 21:22:48 +0800

    feat: 虚拟滚动列表组件1.0

--- vue3-demo/src/App.vue
+++ vue3-demo/src/App.vue
@@ -1,12 +1,50 @@
 <script setup lang="ts">
-import TheWelcome from './components/TheWelcome.vue'
-
+import { ref, reactive, computed, onMounted } from 'vue'
+import VirtuallyList from './components/VirtuallyList.vue'
+type TransferPersonInfo = {
+    name: string
+    phone: string
+    id: string
+    phoneNo: string
+}
+// mock数据
+const getContactList = (): Promise<TransferPersonInfo[]> => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            let list: TransferPersonInfo[] = []
+            for (let i = 0; i < 200; i++) {
+                list.push({
+                    name: '张三' + i,
+                    phone: '1234567890' + i,
+                    id: i + '',
+                    phoneNo: '000' + i
+                })
+            }
+            resolve(list)
+        }, 100)
+    })
+}
+const transferContactList = ref<TransferPersonInfo[]>([]) // 真实数据
+getContactList().then((res) => {
+    transferContactList.value = res
+})
 </script>
 
 <template>
-    <TheWelcome />
+    <div style="height: 400px">
+        <VirtuallyList  :listData="transferContactList" v-slot="slotProps">
+            <div class="item" style="height: 28px;">
+                {{ slotProps.item.phoneNo }}
+            </div>
+        </VirtuallyList>
+    </div>
+
 </template>
 
 <style scoped>
-
+.item{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 </style>
---
+++ vue3-demo/src/components/VirtuallyList.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { computed, nextTick, onMounted, onUpdated, ref } from 'vue'
+
+type TransferPersonInfo = {
+  name: string
+  phone: string
+  id: string
+  phoneNo: string
+}
+const props = defineProps({
+  // 真实数据
+  listData: {
+    type: Array as () => TransferPersonInfo[],
+    default: () => {
+      return []
+    }
+  },
+  // 展示内容区域高度
+  height: {
+    type: String,
+    default: '100%'
+  },
+})
+// item元素
+const items = ref<(Element[])>([])
+// item高度
+const itemHeight = ref(0)
+onUpdated(() => {
+    if (itemHeight.value) return
+    if (items.value && items.value.length) {
+      const { height } = items.value[0].getBoundingClientRect()
+      itemHeight.value = height
+    }
+})
+// 滚动区域高度
+const scrollHeight = computed(() => {
+  return itemHeight.value * props.listData.length
+})
+// 滚动上方距离
+const scrollTop = ref(0)
+// itemcontent的偏移量
+const offsetY = computed(() => {
+  return scrollTop.value - (scrollTop.value % itemHeight.value)
+})
+// 开始索引
+const startIndex = computed(() => {
+  return Math.floor(scrollTop.value / itemHeight.value)
+})
+// 显示区域高度
+const viewContent = ref<Element | null>(null)
+const viewHeight = ref(0)
+onMounted(() => {
+  if (viewContent.value) {
+    viewHeight.value = viewContent.value.clientHeight
+  }
+})
+// 结束索引
+const endIndex = computed(() => {
+  return Math.ceil(startIndex.value + (viewHeight.value / itemHeight.value)) || 1
+})
+// 显示的数据
+const visibleData = computed(() => {
+  return props.listData.slice(startIndex.value, endIndex.value)
+})
+// 滚动事件
+const scrollHandle = (e: Event) => {
+  scrollTop.value = (e.target as Element).scrollTop
+}
+</script>
+
+<template>
+  <div ref="viewContent" class="view-content" @scroll="scrollHandle" :style="{height}">
+    <div class="scroll-content" :style="{ height: scrollHeight + 'px' }">
+      <div class="item-container" :style="{ transform: `translateY(${offsetY}px` }">
+        <div ref="items" class="item" v-for="(item, index) in visibleData" :key="index">
+          <slot :item="item"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.view-content {
+  width: 200px;
+  background-color: #ccc;
+  position: relative;
+  overflow: auto;
+}
+.item-container {
+  overflow: hidden;
+}
+</style>
