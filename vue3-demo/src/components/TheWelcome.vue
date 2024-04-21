3c959f1859e055560b0d2596cc78e6e969821d80
NiuBen <niuben@qq.com>
2024-04-21 12:35:22 +0800

    feat: 虚拟列表1.0

--- vue3-demo/src/components/TheWelcome.vue
+++ vue3-demo/src/components/TheWelcome.vue
@@ -1,88 +1,101 @@
 <script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+import { nextTick, ref } from 'vue'
+type TransferPersonInfo = {
+  name: string
+  phone: string
+  id: string
+  phoneNo: string
+}
+// mock数据
+const getContactList = (): Promise<TransferPersonInfo[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      let list = []
+      for (let i = 0; i < 500000; i++) {
+        list.push({
+          name: '张三' + i,
+          phone: '1234567890' + i,
+          id: i + '',
+          phoneNo: '000' + i
+        })
+      }
+      resolve(list)
+    }, 100)
+  })
+}
+const transferContactList = ref<TransferPersonInfo[]>([])
+const showData = ref<TransferPersonInfo[]>([]) // 显示的数据
+
+getContactList().then((res) => {
+  transferContactList.value = res
+  showData.value = transferContactList.value.slice(0, 20) // 初始展示的数据 （前20个）
+  console.log('---data---', showData.value)
+})
+
+
+const scrollTop = ref(0) // 初始滚动距离
+const itemHeight = ref(0) // 每一个item的高度
+const viewHeight = ref(0) // 容器高度
+
+const getHeight = () => {
+  const item = document.querySelector<HTMLDivElement>('.person-info-card')
+  const view = document.querySelector<HTMLDivElement>('.person-info-view')
+    console.log('---view---', view);
+  console.log('---item---', item);
+
+  itemHeight.value = item ? item.offsetHeight : 0
+  viewHeight.value = view ? view.offsetHeight : 0
+  console.log('itemHeight', itemHeight.value)
+  console.log('viewHeight', viewHeight.value)
+}
+nextTick(getHeight)
+
+
+// 滚动事件
+const handleScroll = (e: Event) => {
+  if (itemHeight.value === 0 || viewHeight.value === 0) {
+    getHeight()
+  }
+  // 获取滚动距离
+  scrollTop.value = (e.target as HTMLElement).scrollTop
+  console.log('scrollTop.value', scrollTop.value)
+  // 初始索引 = 滚动距离 / 每一项的高度
+  const startIndex = Math.floor(scrollTop.value / itemHeight.value)
+  // 结束索引 = 初始索引 + 容器高度 / 每一项的高度
+  const endIndex = Math.ceil(startIndex + 1 + viewHeight.value / itemHeight.value) 
+  // 根据初始索引和结束索引，截取数据
+  showData.value = transferContactList.value.slice(startIndex, endIndex)
+}
+
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+  <div class="view-content person-info-view" @scroll="handleScroll">
+    <div class="contact-content-container" :style="{height: `${transferContactList.length * itemHeight}px`}" >
+      <div class="item-container "
+      :style="{ transform: `translateY(${scrollTop}px)`}">
+        <div class="item person-info-card" v-for="(item, index) in showData" :key="index">
+          {{ item.phoneNo }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<style>
+.view-content {
+  width: 200px;
+  height: 400px;
+  background-color: #ccc;
+  position: relative;
+  overflow: auto;
+}
+
+.contact-content-container{
+  height: 1000px;
+}
+.item-container{
+  position: absolute;
+  overflow: hidden;
+}
+
+</style>
